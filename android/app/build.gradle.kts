bf7961e51bb00a2234e894e5e29923d1fb06d445
tushar <tushar@gmil.>
2026-09-25 03:44:12 +0530

    update biuld.gradle

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -71,6 +71,11 @@ android {
         }
     }
 
+    lint {
+        abortOnError = false
+        checkReleaseBuilds = false
+    }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
